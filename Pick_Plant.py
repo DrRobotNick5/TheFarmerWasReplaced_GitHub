9eb64e7b1542ffb690f15194f56d66494f3242f0
DrRobotNick5 <creepercraft5555@gmail.com>
2025-10-31 17:32:28 -0700

    Added Pumpkin logic

--- Pick_Plant.py
+++ Pick_Plant.py
@@ -1,15 +1,48 @@
-def plant_logic(num_row_plants):
+def plant_logic(num_row_plants, pumpkin_tally):
 	x = get_pos_x()
 	y = get_pos_y()
-	if  (x+y) % num_row_plants == 0:
-		if get_ground_type() == Grounds.Soil:
-			till()
-		plant(Entities.Grass)
-	if (x+y) % num_row_plants == 1:
-		if get_ground_type() == Grounds.Soil:
-			till()
-		plant(Entities.Tree)
-	if (x+y) % num_row_plants == 2:
+	size = get_world_size()
+
+	if size/2 > 6:
+		max_pumpkin = 6
+	else:
+		max_pumpkin = size/2
+
+
+	if x<max_pumpkin and y<max_pumpkin:
+		if get_entity_type() != Entities.Pumpkin:
+			if can_harvest():
+				harvest()
+			pumpkin_tally = 0
+		else:
+			pumpkin_tally +=1
+			
+		if pumpkin_tally == 9:
+			harvest()
+			pumpkin_tally = 0
+
 		if get_ground_type() == Grounds.Grassland:
 			till()
-		plant(Entities.Carrot)
+
+		plant(Entities.Pumpkin)
+
+
+
+	else:
+		if can_harvest():
+			harvest()
+		
+		if  (x+y) % num_row_plants == 0:
+			if get_ground_type() == Grounds.Soil:
+				till()
+			plant(Entities.Grass)
+		if (x+y) % num_row_plants == 1:
+			if get_ground_type() == Grounds.Soil:
+				till()
+			plant(Entities.Tree)
+		if (x+y) % num_row_plants == 2:
+			if get_ground_type() == Grounds.Grassland:
+				till()
+			plant(Entities.Carrot)
+	
+	return pumpkin_tally
--- main.py
+++ main.py
@@ -5,18 +5,13 @@ num_row_plants = 3
 number_of_plants = 4
 
 dirValue = 0
+pumpkin_tally = 0
 
 while 1:
 	
 	dirValue = Movement.Movement(dirValue)
 	
-	if can_harvest():
-		harvest()
-		Pick_Plant.plant_logic(num_row_plants)
-	if get_entity_type() == None:
-		Pick_Plant.plant_logic(num_row_plants)
-		
-			
+	pumpkin_tally = Pick_Plant.plant_logic(num_row_plants, pumpkin_tally)
 	
 	if get_water() < .75:
 		use_item(Items.Water)
