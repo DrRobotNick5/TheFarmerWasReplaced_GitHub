1c638f27ff9601c0f445311ab79bc772df637383
DrRobotNick5 <creepercraft5555@gmail.com>
2025-11-14 23:29:19 -0800

    working Hamiltonian hash generator

--- Snake_V2.py
+++ Snake_V2.py
@@ -1,8 +1,10 @@
 def update_snake_array():
 	
 	snake_array.append([get_pos_x(),get_pos_y()])
+	snake_set.add((get_pos_x(),get_pos_y()))
 	
 	if len(snake_array) > length+tail_buffer:
+		snake_set.remove(snake_array(0))
 		snake_array.pop(0)
 		
 def check_move(direction):
@@ -26,11 +28,57 @@ direction_x = apple_x - x
 direction_y = apple_y - y
 
 snake_array = []
+snake_set = set()
+hamltonian = {}
 length = 1
 tail_buffer = 3
 end = False
 
 To_Center = True
+
+temp_x = 0
+temp_y = 0
+
+for i in range(world**2):
+	
+	hamltonian[i] = (temp_x,temp_y)
+	
+	even_col = temp_x % 2 == 0
+	odd_col = temp_x % 2 == 1
+	
+	top_half = temp_y >= (world-1)//2+1
+	bottom_half = temp_y <= (world-1)//2
+	top_map = temp_y == world-1
+	bottom_map = temp_y == 0
+	top_partition = temp_y == (world-1)//2
+	bottom_partition = temp_y == (world-1)//2+1
+	
+	if temp_x == 0 and temp_y != world-1:
+		temp_y += 1
+	elif temp_x == 0 and temp_y == world-1:
+		temp_x += 1
+	elif temp_x == world-1 and temp_y != 0:
+		temp_y -= 1
+	elif temp_x == world-1 and temp_y != 0:
+		temp_x -= 1
+
+	elif odd_col and bottom_map:
+		temp_x -= 1
+	elif even_col and top_partition:
+		 temp_x -= 1
+	elif even_col and top_map:
+		temp_x += 1	
+	elif odd_col and bottom_partition:
+		temp_x += 1
+		
+	elif odd_col:
+		temp_y -= 1
+	elif even_col:
+		temp_y += 1
+	
+		
+quick_print(hamltonian)
+	
 
 while 1:
 	
